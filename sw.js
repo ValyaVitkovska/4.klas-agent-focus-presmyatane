3399c81c0675d0897034d25ebaa5403c43420585
Валя Витковска <valia.vitkovska@gmail.com>
2026-08-11 07:01:28 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='agent-focus-v9';
+const CACHE_NAME='agent-focus-v10';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./assets/agent-focus.png','./assets/agent-focus-portrait.png','./assets/valya-logo.png','./icons/icon-192.png','./icons/icon-512.png','./icons/icon-maskable-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
